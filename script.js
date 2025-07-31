5234d5137ab07191df11384559ba5d2a00b003bd
haseebsgit <haseebfarooq1111@gmail.com>
2025-08-01 01:32:55 +0500

    third change

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      {},
-      "i am an h1 tag, child of div child which is the child of div parent"
-    ),
-    React.createElement(
-      "h2",
-      {},
-      "i am an h2 tag, child of div child which is child of div parent"
-    ),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      {},
-      "i am an h1 tag, child of div child which is the child of div parent"
-    ),
-    React.createElement(
-      "h2",
-      {},
-      "i am an h2 tag, child of div child which is child of div parent"
-    ),
-  ]),
-]);
+  React.createElement(
+    "div",
+    { id: "child", key: "child-1" },
+    [
+      React.createElement("h1", { key: "h1-1" }, "Haseeb Farooq"),
+      React.createElement("h2", { key: "h2-1" }, "React"),
+      React.createElement("h3", { key: "h3-1" }, "JavaScript"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2", key: "child-2" },
+    [
+      React.createElement("h1", { key: "h1-2" }, "Haseeb"),
+      React.createElement("h2", { key: "h2-2" }, "React"),
+      React.createElement("h3", { key: "h3-2" }, "JavaScript"),
+    ]
+  ),
+]); 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render (parent);
- 
+root.render(parent);
