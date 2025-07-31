4921f053976847b1d61c637826226471f4bd6cd8
haseebsgit <haseebfarooq1111@gmail.com>
2025-08-01 01:32:55 +0500

    third change

---
+++ dist/react.672d4772.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "i am an h1 tag, child of div child which is the child of div parent"),
+        React.createElement("h2", {}, "i am an h2 tag, child of div child which is child of div parent")
+    ]),
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "i am an h1 tag, child of div child which is the child of div parent"),
+        React.createElement("h2", {}, "i am an h2 tag, child of div child which is child of div parent")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=react.672d4772.js.map
