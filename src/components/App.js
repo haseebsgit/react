a39b258ed91d872a9640b2f610f607e07119f3ad
haseebsgit <haseebfarooq1111@gmail.com>
2025-08-06 18:44:21 +0500

    separate components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import Body from "./Body";
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+);
+
+export default App;
